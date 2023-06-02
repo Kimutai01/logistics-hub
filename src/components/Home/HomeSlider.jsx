20be4e83af033575adbe5fb1084e7cd8da57cf1a
MICHAELMUNAVU83 <michaelmunavu83.com>
2023-06-02 12:30:38 +0300

    Setup

--- src/components/Home/HomeSlider.jsx
+++ src/components/Home/HomeSlider.jsx
@@ -1,22 +1,40 @@
 import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
+import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
 
 // Import Swiper styles
 import "swiper/css";
-import "swiper/css/navigation";
-import "./HomeSlider.css";
 import Slider1 from "/slider1.png";
 import Slider2 from "/slider2.png";
 import Slider3 from "/slider3.png";
+import arrowbg from "/arrowbg.png";
 
 // import required modules
-import { Navigation } from "swiper";
+
 const HomeSlider = () => {
+  const swipe = () => {
+    const swiper = document.querySelector(".swiper-container").swiper;
+    swiper.slideNext();
+  };
+
+  const swipeBack = () => {
+    const swiper = document.querySelector(".swiper-container").swiper;
+    swiper.slidePrev();
+  };
   return (
     <div>
-      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-        <SwiperSlide className="flex gap-10 px-20 items-center">
+      <Swiper className="swiper-container" loop={true}>
+        <SwiperSlide className="flex justify-between   items-center">
+          <p
+            className="bg-no-repeat cursor-pointer flex items-center justify-center w-[60px] h-[446px] bg-center bg-contain text-4xl font-bold text-white"
+            style={{
+              backgroundImage: `url(${arrowbg})`,
+            }}
+            onClick={swipeBack}
+          >
+            <IoIosArrowBack />
+          </p>
           <img src={Slider1} alt="slider1" className="py-10" />
           <div>
             <h3 className="font-sans font-bold text-2xl mb-10">
@@ -37,53 +55,26 @@ const HomeSlider = () => {
               make an inquiry
             </button>
           </div>
+          <p
+            className="bg-no-repeat flex items-center cursor-pointer justify-center w-[60px] h-[446px] bg-center bg-contain text-4xl font-bold text-white"
+            style={{
+              backgroundImage: `url(${arrowbg})`,
+            }}
+            onClick={swipe}
+          >
+            <IoIosArrowForward />
+          </p>
         </SwiperSlide>
-        <SwiperSlide className="flex gap-10 px-20 items-center">
-          <img src={Slider1} alt="slider1" className="py-10" />
-          <div>
-            <h3 className="font-sans font-bold text-2xl mb-10">
-              Freight logistics
-            </h3>
-            <p className="font-light text-xl uppercase mb-5">
-              We provide the following freight logistics services:
-            </p>
-            <ul className="font-light text-xl list-disc list-inside uppercase">
-              <li>Air and ocean transport</li>
-              <li>Import and export</li>
-              <li>Break bulk and worldwide consolidation</li>
-              <li>Perishable logistics</li>
-              <li>Comprehensive quotation</li>
-              <li>Consultation</li>
-              <li>Project logistics</li>
-            </ul>
-            <button className="bg-gradient-to-b from-blue-900 to-pink-300 uppercase p-3 rounded-xl text-white mt-10">
-              make an inquiry
-            </button>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="flex gap-10 px-20 items-center">
-          <img src={Slider2} alt="slider1" className="py-5" />
-          <div>
-            <h3 className="font-sans font-bold text-2xl mb-10">
-              Tracks and Trailers
-            </h3>
-            <p className="font-light text-xl uppercase mb-5">
-              Our trailers consist of:
-            </p>
-            <ul className="font-light text-xl list-disc list-inside uppercase">
-              <li>Steel and coil carriers</li>
-              <li>Flatbed trailers</li>
-              <li>Tipper trailers</li>
-              <li>LPG tanker trailers</li>
-              <li>Standard low loaders</li>
-              <li>Heavy lift modular trailers</li>
-            </ul>
-            <button className="bg-gradient-to-b from-blue-900 to-pink-300 uppercase p-3 rounded-xl text-white mt-10">
-              make an inquiry
-            </button>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="flex gap-10 px-20 items-center">
+        <SwiperSlide className="flex justify-between   items-center">
+          <p
+            className="bg-no-repeat cursor-pointer flex items-center justify-center w-[60px] h-[446px] bg-center bg-contain text-4xl font-bold text-white"
+            style={{
+              backgroundImage: `url(${arrowbg})`,
+            }}
+            onClick={swipeBack}
+          >
+            <IoIosArrowBack />
+          </p>
           <img src={Slider1} alt="slider1" className="py-10" />
           <div>
             <h3 className="font-sans font-bold text-2xl mb-10">
@@ -104,12 +95,136 @@ const HomeSlider = () => {
               make an inquiry
             </button>
           </div>
+          <p
+            className="bg-no-repeat flex items-center cursor-pointer justify-center w-[60px] h-[446px] bg-center bg-contain text-4xl font-bold text-white"
+            style={{
+              backgroundImage: `url(${arrowbg})`,
+            }}
+            onClick={swipe}
+          >
+            <IoIosArrowForward />
+          </p>
         </SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        <SwiperSlide className="flex justify-between   items-center">
+          <p
+            className="bg-no-repeat cursor-pointer flex items-center justify-center w-[60px] h-[446px] bg-center bg-contain text-4xl font-bold text-white"
+            style={{
+              backgroundImage: `url(${arrowbg})`,
+            }}
+            onClick={swipeBack}
+          >
+            <IoIosArrowBack />
+          </p>
+          <img src={Slider1} alt="slider1" className="py-10" />
+          <div>
+            <h3 className="font-sans font-bold text-2xl mb-10">
+              Tracks and Trailers
+            </h3>
+            <p className="font-light text-xl uppercase mb-5">
+              Our trailers consist of:
+            </p>
+            <ul className="font-light text-xl list-disc list-inside uppercase">
+              <li>Steel and coil carriers</li>
+              <li>Flatbed trailers</li>
+              <li>Tipper trailers</li>
+              <li>LPG tanker trailers</li>
+              <li>Standard low loaders</li>
+              <li>Heavy lift modular trailers</li>
+            </ul>
+            <button className="bg-gradient-to-b from-blue-900 to-pink-300 uppercase p-3 rounded-xl text-white mt-10">
+              make an inquiry
+            </button>
+          </div>
+          <p
+            className="bg-no-repeat flex items-center cursor-pointer justify-center w-[60px] h-[446px] bg-center bg-contain text-4xl font-bold text-white"
+            style={{
+              backgroundImage: `url(${arrowbg})`,
+            }}
+            onClick={swipe}
+          >
+            <IoIosArrowForward />
+          </p>
+        </SwiperSlide>
+        <SwiperSlide className="flex justify-between   items-center">
+          <p
+            className="bg-no-repeat cursor-pointer flex items-center justify-center w-[60px] h-[446px] bg-center bg-contain text-4xl font-bold text-white"
+            style={{
+              backgroundImage: `url(${arrowbg})`,
+            }}
+            onClick={swipeBack}
+          >
+            <IoIosArrowBack />
+          </p>
+          <img src={Slider1} alt="slider1" className="py-10" />
+          <div>
+            <h3 className="font-sans font-bold text-2xl mb-10">
+              Tracks and Trailers
+            </h3>
+            <p className="font-light text-xl uppercase mb-5">
+              Our trailers consist of:
+            </p>
+            <ul className="font-light text-xl list-disc list-inside uppercase">
+              <li>Steel and coil carriers</li>
+              <li>Flatbed trailers</li>
+              <li>Tipper trailers</li>
+              <li>LPG tanker trailers</li>
+              <li>Standard low loaders</li>
+              <li>Heavy lift modular trailers</li>
+            </ul>
+            <button className="bg-gradient-to-b from-blue-900 to-pink-300 uppercase p-3 rounded-xl text-white mt-10">
+              make an inquiry
+            </button>
+          </div>
+          <p
+            className="bg-no-repeat flex items-center cursor-pointer justify-center w-[60px] h-[446px] bg-center bg-contain text-4xl font-bold text-white"
+            style={{
+              backgroundImage: `url(${arrowbg})`,
+            }}
+            onClick={swipe}
+          >
+            <IoIosArrowForward />
+          </p>
+        </SwiperSlide>
+        <SwiperSlide className="flex justify-between   items-center">
+          <p
+            className="bg-no-repeat cursor-pointer flex items-center justify-center w-[60px] h-[446px] bg-center bg-contain text-4xl font-bold text-white"
+            style={{
+              backgroundImage: `url(${arrowbg})`,
+            }}
+            onClick={swipeBack}
+          >
+            <IoIosArrowBack />
+          </p>
+          <img src={Slider1} alt="slider1" className="py-10" />
+          <div>
+            <h3 className="font-sans font-bold text-2xl mb-10">
+              Tracks and Trailers
+            </h3>
+            <p className="font-light text-xl uppercase mb-5">
+              Our trailers consist of:
+            </p>
+            <ul className="font-light text-xl list-disc list-inside uppercase">
+              <li>Steel and coil carriers</li>
+              <li>Flatbed trailers</li>
+              <li>Tipper trailers</li>
+              <li>LPG tanker trailers</li>
+              <li>Standard low loaders</li>
+              <li>Heavy lift modular trailers</li>
+            </ul>
+            <button className="bg-gradient-to-b from-blue-900 to-pink-300 uppercase p-3 rounded-xl text-white mt-10">
+              make an inquiry
+            </button>
+          </div>
+          <p
+            className="bg-no-repeat flex items-center cursor-pointer justify-center w-[60px] h-[446px] bg-center bg-contain text-4xl font-bold text-white"
+            style={{
+              backgroundImage: `url(${arrowbg})`,
+            }}
+            onClick={swipe}
+          >
+            <IoIosArrowForward />
+          </p>
+        </SwiperSlide>
       </Swiper>
     </div>
   );
--- src/components/Home/ItemsSlider.jsx
+++ src/components/Home/ItemsSlider.jsx
@@ -1,43 +1,16 @@
 import React from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
-import "./HomeSlider.css";
+
 import Slider1 from "/slider1.png";
 import Slider2 from "/slider2.png";
 import Slider3 from "/slider3.png";
 
 const ItemsSlider = () => {
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 7,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 1500,
-    cssEase: "linear",
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
-  };
   const items = [
     { id: 1, text: "JINJA" },
     { id: 2, text: "KAMPALA" },
@@ -53,19 +26,24 @@ const ItemsSlider = () => {
 
   return (
     <div className="mt-10">
-      <Slider
-        {...settings}
-        className=" w-full bg-gradient-to-r from-blue-900 to-pink-300 px-4"
+      <Swiper
+        slidesPerView={7}
+        spaceBetween={30}
+        slidesPerGroup={1}
+        loop={true}
+        className=" w-[100%] bg-gradient-to-r from-blue-900 to-pink-300 "
       >
         {items.map((item) => (
           <>
-            <div key={item.id} className="flex items-center">
-              <div className="w-1 h-10 bg-gray-400"></div>
-              <h2 className="text-xl mx-10">{item.text}</h2>
-            </div>
+            <SwiperSlide className="flex justify-center items-center">
+              <div key={item.id} className="flex justify-center items-center">
+                <div className="w-1 h-10 bg-gray-400"></div>
+                <h2 className="text-xl mx-10">{item.text}</h2>
+              </div>
+            </SwiperSlide>
           </>
         ))}
-      </Slider>
+      </Swiper>
     </div>
   );
 };
